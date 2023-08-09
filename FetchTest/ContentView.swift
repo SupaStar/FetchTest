08e04e489d1e68974fefe5d0c137fd99f3ac1f49
Obed Noe Martinez Gonzalez <68682799+SupaStar@users.noreply.github.com>
2023-08-09 15:08:12 -0600

    Initial Commit

---
+++ FetchTest/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView
+//  FetchTest
+//
+//  Created by Obed Martinez on 09/08/23
+//
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchTest/FetchTestApp.swift
@@ -0,0 +1,19 @@
+//
+//  FetchTestApp
+//  FetchTest
+//
+//  Created by Obed Martinez on 09/08/23
+//
+
+
+
+import SwiftUI
+
+@main
+struct FetchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
